0b90138472aa83172ee8a476e2740c935dfeac0e
freshie-Dev <hazardpubg1122@gmail.com>
2023-09-03 20:51:50 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>My name is Ahmad Ali</h1>
+      <h2>This is going to be a great E-commerce website in 1 week</h2>
+    </div>
+  )
+}
+
+export default App
